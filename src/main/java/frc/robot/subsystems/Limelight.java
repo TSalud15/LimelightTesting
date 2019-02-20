d0c7ff0fda99d18ea8c7a6abeab9cac42af4ce91
TSalud15 <saludtravis15@gmail.com>
2019-02-19 16:33:56 -0800

    first commit

---
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class Limelight extends Subsystem {
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx;
+  NetworkTableEntry ty;
+  NetworkTableEntry ta;
+  NetworkTableEntry pipeline;
+  
+  public Limelight() {
+    tx = table.getEntry("tx");
+    ty = table.getEntry("ty");
+    ta = table.getEntry("ta");
+    pipeline = table.getEntry("pipeline");
+
+    pipeline.setValue(1);
+  }
+
+  public double getXOffsetFromTarget() {
+    double x = tx.getDouble(0.0);
+    return x;
+  }
+
+  public double getYOffsetFromTarget() {
+    double y = ty.getDouble(0.0);
+    return y;
+  }
+
+  public double getTargetArea() {
+    double area = ta.getDouble(0.0);
+    return area;
+  }
+
+  public void reportToSmartDashboard() {
+    SmartDashboard.putNumber("Limelight X", getXOffsetFromTarget());
+    SmartDashboard.putNumber("Limelight Y", getYOffsetFromTarget());
+    SmartDashboard.putNumber("Limelight Area", getTargetArea());
+  }
+  
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
